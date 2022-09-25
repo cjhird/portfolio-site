890700a1b1a31e5e28c505d5e4f49bc6943fe575
cjhird <charlie.hird@googlemail.com>
2022-09-25 21:44:59 +0100

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Landing from './components/Landing.js'
+import NotFound from './components/NotFound.js'
+import SiteNavBar from './components/SiteNavBar.js'
 
 function App() {
-  return <></>
+  return (
+    <div className="site-wrapper">
+      <BrowserRouter>
+        <SiteNavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/SiteNavBar.js
@@ -0,0 +1,13 @@
+// Import Link
+import { useNavigate, Link } from 'react-router-dom'
+
+// Import React Bootstrap Components
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+const SiteNavBar = () => {
+  return <Navbar bg="dark" variant="dark" expand="lg"></Navbar>
+}
+
+export default SiteNavBar
