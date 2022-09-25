1375222df90340c806cae42515c266fe14e9e074
cjhird <charlie.hird@googlemail.com>
2022-09-25 21:39:38 +0100

    changes to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/main.scss'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+createRoot(document.getElementById('root')).render(<App />)
